754568001b2081b12558191fb719044c5770fd27
mieltn <mike.elyutin@gmail.com>
2022-10-01 12:05:40 +0500

    added generalstat api view

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import Clients, MailingLists
+from .views import Clients, GeneralStat, MailingLists
 
 urlpatterns = [
     path('clients/', Clients.as_view()),
     path('clients/<int:id>', Clients.as_view()),
     path('mailinglists/', MailingLists.as_view()),
     path('mailinglists/<int:id>', MailingLists.as_view()),
+    path('generalstat/', GeneralStat.as_view()),
+    # path('detailedstat/', DetailedStat.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -1,16 +1,18 @@
 from rest_framework.views import APIView
 from django.http import JsonResponse
+from django.db.models import Count
+
 from .models import MailingList, Client
 from .serializers import MailingListSerializer, ClientSerializer
 # from django.core import serializers
 
 from notifserv.celery import app
 
-from .tasks import scheduleMailingTask, sendMessageTask
+from .tasks import scheduleMailingTask
 
-from datetime import datetime
-from pytz import timezone
-from django.utils import timezone
+# from datetime import datetime
+# from pytz import timezone
+# from django.utils import timezone
 
 
 class Clients(APIView):
@@ -68,14 +70,12 @@ class MailingLists(APIView):
         return JsonResponse({'message': 'failed to create new mailing list', 'object': serializer.data, 'error': serializer.errors})
 
 
-
     def patch(self, request, id):
         mailinglist = MailingList.objects.get(pk=id)
 
         serializer = MailingListSerializer(mailinglist, data=request.data, partial=True)
         if serializer.is_valid():
-            taskOld = mailinglist.taskId
-            app.control.revoke(taskOld, terminate=True)
+            app.control.revoke(mailinglist.taskId, terminate=True)
 
             mailinglist = serializer.save()
 
@@ -92,14 +92,29 @@ class MailingLists(APIView):
         return JsonResponse({'message': 'failed to update the mailing list', 'object': serializer.data, 'error': serializer.errors})
             
 
+    def delete(self, request, id):
+        mailinglist = MailingList.objects.get(pk=id)
+        app.control.revoke(mailinglist.taskId, terminate=True)
+        mailinglist.delete()
+
+        serializer = MailingListSerializer(mailinglist)
+
+        return JsonResponse({'message': 'successfully deleted the mailing list', 'object': serializer.data})
 
 
-    # def delete(self, request, id):
 
+class GeneralStat(APIView):
+    def get(self, request):
+        stat = {}
+        mailings = MailingList.objects.all()
+        stat['total number of mailings'] = mailings.count()
 
-# class GeneralStat(APIView):
-#     def get(self, request):
+        for mailing in mailings:
+            messages = mailing.message_set.select_related()
+            msStat = list(messages.values('status').annotate(messagesSent=Count('id')))
+            stat[mailing.id] = msStat
 
+        return JsonResponse({'message': 'General statistics about mailings and messages sent by status', 'data': stat})
 
 
 # class DetailedStat(APIView):
